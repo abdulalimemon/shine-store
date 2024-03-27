d182d486b54f69345f591d82cdf9d444be3feaea
emon02 <abdulalimemon11@gmail.com>
2024-03-28 01:44:38 +0600

    added flash sale component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,20 @@ import Navbar from "@/components/layout/main/Navbar";
 import LoadingPage from "@/components/layout/main/Loading";
 import HeroSlider from "@/components/component/home/HeroSlider";
 
-export default function Home() {
+const HomePage = async () => {
+  const res = await fetch(`${process.env.BACKEND_URL}/product`, {
+    next: { revalidate: 30 },
+  });
+  const data = await res.json();
+  console.log(data);
   return (
     <main>
-      <Navbar/>
-      <HeroSlider/>
-      <LoadingPage/>
-      <Footer/>
+      <Navbar />
+      <HeroSlider />
+      <LoadingPage />
+      <Footer />
     </main>
   );
-}
+};
+
+export default HomePage;
---
+++ src/components/component/home/FlashSale.tsx
@@ -0,0 +1,18 @@
+import Container from "@/components/layout/Container";
+import { Button } from "@/components/ui/button";
+
+const FlashSale = () => {
+  return (
+    <section>
+      <Container>
+        <div>
+          <h2>Flash Sale</h2>
+          <Button>View All</Button>
+        </div>
+        <div></div>
+      </Container>
+    </section>
+  );
+};
+
+export default FlashSale;
