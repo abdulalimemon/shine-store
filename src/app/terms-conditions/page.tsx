4c93a7977c0e71a210d351c955e320265abfe5e6
emon02 <abdulalimemon11@gmail.com>
2024-05-30 00:40:26 +0600

    complete 2 page

--- src/app/terms-conditions/page.tsx
+++ src/app/terms-conditions/page.tsx
@@ -8,30 +8,30 @@ const TermsConditionsPage = () => {
           <strong>Terms and Conditions</strong>
         </h2>
 
-        <p>Welcome to Shine Store!</p>
+        <p className="text-sm lg:text-base pb-5">Welcome to Shine Store!</p>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           These terms and conditions outline the rules and regulations for the
           use of Shine Store&apos;s Website, located at
           https://shine-store.vercel.app/.
         </p>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           By accessing this website we assume you accept these terms and
           conditions. Do not continue to use Shine Store if you do not agree to
           take all of the terms and conditions stated on this page.
         </p>
 
-        <h3>
+        <h3 className="text-base lg:text-xl font-semibold pt-5">
           <strong>Cookies</strong>
         </h3>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           We employ the use of cookies. By accessing Shine Store, you agreed to
           use cookies in agreement with the Shine Store&apos;s Privacy Policy.{" "}
         </p>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           Most interactive websites use cookies to let us retrieve the
           user&apos;s details for each visit. Cookies are used by our website to
           enable the functionality of certain areas to make it easier for people
@@ -39,11 +39,11 @@ const TermsConditionsPage = () => {
           also use cookies.
         </p>
 
-        <h3>
+        <h3 className="text-base lg:text-xl font-semibold pt-5">
           <strong>License</strong>
         </h3>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           Unless otherwise stated, Shine Store and/or its licensors own the
           intellectual property rights for all material on Shine Store. All
           intellectual property rights are reserved. You may access this from
@@ -51,24 +51,15 @@ const TermsConditionsPage = () => {
           these terms and conditions.
         </p>
 
-        <p>You must not:</p>
-        <ul>
+        <p className="text-sm lg:text-base pb-5">You must not:</p>
+        <ul className="space-y-3 font-medium">
           <li>Republish material from Shine Store</li>
           <li>Sell, rent or sub-license material from Shine Store</li>
           <li>Reproduce, duplicate or copy material from Shine Store</li>
           <li>Redistribute content from Shine Store</li>
-        </ul>
+        </ul>       
 
-        <p>
-          This Agreement shall begin on the date hereof. Our Terms and
-          Conditions were created with the help of the{" "}
-          <a href="https://www.termsandconditionsgenerator.com/">
-            Free Terms and Conditions Generator
-          </a>
-          .
-        </p>
-
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           Parts of this website offer an opportunity for users to post and
           exchange opinions and information in certain areas of the website.
           Shine Store does not filter, edit, publish or review Comments prior to
@@ -81,15 +72,15 @@ const TermsConditionsPage = () => {
           posting of and/or appearance of the Comments on this website.
         </p>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           Shine Store reserves the right to monitor all Comments and to remove
           any Comments which can be considered inappropriate, offensive or
           causes breach of these Terms and Conditions.
         </p>
 
-        <p>You warrant and represent that:</p>
+        <p className="text-sm lg:text-base pb-5">You warrant and represent that:</p>
 
-        <ul>
+        <ul className="space-y-3 font-medium">
           <li>
             You are entitled to post the Comments on our website and have all
             necessary licenses and consents to do so;
@@ -110,22 +101,22 @@ const TermsConditionsPage = () => {
           </li>
         </ul>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           You hereby grant Shine Store a non-exclusive license to use,
           reproduce, edit and authorize others to use, reproduce and edit any of
           your Comments in any and all forms, formats or media.
         </p>
 
-        <h3>
+        <h3 className="text-base lg:text-xl font-semibold pt-5">
           <strong>Hyperlinking to our Content</strong>
         </h3>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           The following organizations may link to our Website without prior
           written approval:
         </p>
 
-        <ul>
+        <ul className="space-y-3 font-medium">
           <li>Government agencies;</li>
           <li>Search engines;</li>
           <li>News organizations;</li>
@@ -141,7 +132,7 @@ const TermsConditionsPage = () => {
           </li>
         </ul>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           These organizations may link to our home page, to publications or to
           other Website information so long as the link: (a) is not in any way
           deceptive; (b) does not falsely imply sponsorship, endorsement or
@@ -149,12 +140,12 @@ const TermsConditionsPage = () => {
           (c) fits within the context of the linking party&apos;s site.
         </p>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           We may consider and approve other link requests from the following
           types of organizations:
         </p>
 
-        <ul>
+        <ul className="space-y-3 font-medium">
           <li>commonly-known consumer and/or business information sources;</li>
           <li>dot.com community sites;</li>
           <li>associations or other groups representing charities;</li>
@@ -164,7 +155,7 @@ const TermsConditionsPage = () => {
           <li>educational institutions and trade associations.</li>
         </ul>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           We will approve link requests from these organizations if we decide
           that: (a) the link would not make us look unfavorably to ourselves or
           to our accredited businesses; (b) the organization does not have any
@@ -173,7 +164,7 @@ const TermsConditionsPage = () => {
           is in the context of general resource information.
         </p>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           These organizations may link to our home page so long as the link: (a)
           is not in any way deceptive; (b) does not falsely imply sponsorship,
           endorsement or approval of the linking party and its products or
@@ -181,7 +172,7 @@ const TermsConditionsPage = () => {
           site.
         </p>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           If you are one of the organizations listed in paragraph 2 above and
           are interested in linking to our website, you must inform us by
           sending an e-mail to Shine Store. Please include your name, your
@@ -191,9 +182,9 @@ const TermsConditionsPage = () => {
           Wait 2-3 weeks for a response.
         </p>
 
-        <p>Approved organizations may hyperlink to our Website as follows:</p>
+        <p className="text-sm lg:text-base pb-5">Approved organizations may hyperlink to our Website as follows:</p>
 
-        <ul>
+        <ul className="space-y-3 font-medium">
           <li>By use of our corporate name; or</li>
           <li>By use of the uniform resource locator being linked to; or</li>
           <li>
@@ -203,26 +194,26 @@ const TermsConditionsPage = () => {
           </li>
         </ul>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           No use of Shine Store&apos;s logo or other artwork will be allowed for
           linking absent a trademark license agreement.
         </p>
 
-        <h3>
+        <h3 className="text-base lg:text-xl font-semibold pt-5">
           <strong>iFrames</strong>
         </h3>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           Without prior approval and written permission, you may not create
           frames around our Webpages that alter in any way the visual
           presentation or appearance of our Website.
         </p>
 
-        <h3>
+        <h3 className="text-base lg:text-xl font-semibold pt-5">
           <strong>Content Liability</strong>
         </h3>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           We shall not be hold responsible for any content that appears on your
           Website. You agree to protect and defend us against all claims that is
           rising on your Website. No link(s) should appear on any Website that
@@ -231,11 +222,11 @@ const TermsConditionsPage = () => {
           violation of, any third party rights.
         </p>
 
-        <h3>
+        <h3 className="text-base lg:text-xl font-semibold pt-5">
           <strong>Reservation of Rights</strong>
         </h3>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           We reserve the right to request that you remove all links or any
           particular link to our Website. You approve to immediately remove all
           links to our Website upon request. We also reserve the right to amen
@@ -244,35 +235,35 @@ const TermsConditionsPage = () => {
           follow these linking terms and conditions.
         </p>
 
-        <h3>
+        <h3 className="text-base lg:text-xl font-semibold pt-5">
           <strong>Removal of links from our website</strong>
         </h3>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           If you find any link on our Website that is offensive for any reason,
           you are free to contact and inform us any moment. We will consider
           requests to remove links but we are not obligated to or so or to
           respond to you directly.
         </p>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           We do not ensure that the information on this website is correct, we
           do not warrant its completeness or accuracy; nor do we promise to
           ensure that the website remains available or that the material on the
           website is kept up to date.
         </p>
 
-        <h3>
+        <h3 className="text-base lg:text-xl font-semibold pt-5">
           <strong>Disclaimer</strong>
         </h3>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           To the maximum extent permitted by applicable law, we exclude all
           representations, warranties and conditions relating to our website and
           the use of this website. Nothing in this disclaimer will:
         </p>
 
-        <ul>
+        <ul className="space-y-3 font-medium">
           <li>
             limit or exclude our or your liability for death or personal injury;
           </li>
@@ -290,7 +281,7 @@ const TermsConditionsPage = () => {
           </li>
         </ul>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           The limitations and prohibitions of liability set in this Section and
           elsewhere in this disclaimer: (a) are subject to the preceding
           paragraph; and (b) govern all liabilities arising under the
@@ -298,7 +289,7 @@ const TermsConditionsPage = () => {
           breach of statutory duty.
         </p>
 
-        <p>
+        <p className="text-sm lg:text-base pb-5">
           As long as the website and the information and services on the website
           are provided free of charge, we will not be liable for any loss or
           damage of any nature.
