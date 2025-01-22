5b17cfb3af7b672dd4b2784e5604e482fa50a905
emon02 <abdulalimemon11@gmail.com>
2025-01-22 23:08:57 +0600

    added modal on user profile page

--- src/app/(withUser)/user/profile/page.tsx
+++ src/app/(withUser)/user/profile/page.tsx
@@ -1,7 +1,32 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import ProfileModal from "./ProfileModal";
+
 const Page = () => {
   return (
-    <div>
-      <h2>Welcome to the Page page</h2>
+    <div className="p-5">
+      <div>
+        <div className="flex justify-between items-center">
+          <h2 className="text-xl font-semibold">Account Information</h2>
+          <ProfileModal />
+        </div>
+        <div>
+          <form className="grid grid-cols-1 md:grid-cols-2 gap-4 py-4">
+            <div>
+              <Label>Name</Label>
+              <Input value="Abdul Alim Emon" readOnly className="mt-3" />
+            </div>
+            <div>
+              <Label>Email</Label>
+              <Input
+                value="abdulalimemon02@gmail.com"
+                readOnly
+                className="mt-3"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
