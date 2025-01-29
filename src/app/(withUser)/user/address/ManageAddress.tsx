0474cb7a69304a76fe629342c3ba226005a19687
emon02 <abdulalimemon11@gmail.com>
2025-01-29 23:09:26 +0600

    complete address page design

--- src/app/(withUser)/user/address/ManageAddress.tsx
+++ src/app/(withUser)/user/address/ManageAddress.tsx
@@ -1,3 +1,4 @@
+import EditAddressModal from "./EditAddressModal";
 import NewAddressModal from "./NewAddressModal";
 
 const ManageAddress = () => {
@@ -7,8 +8,30 @@ const ManageAddress = () => {
         <h2 className="text-xl font-semibold">Manage Addresses</h2>
         <NewAddressModal />
       </div>
-      <div>
+      <div className="my-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-4">
+          <div className="p-5 bg-green-100 space-y-2">
+            <h2 className="text-lg font-bold">Default Shipping & Billing Address</h2>
+            <p className="text-sm font-medium capitalize">Abdul Alim Emon</p>
+            <p className="text-sm font-medium capitalize">0172222222</p>
+            <p className="text-sm font-medium capitalize">mithapukur, Rangpur</p>
+            <p className="text-sm font-medium capitalize">mithapukur, Rangpur</p>
+            <div className="py-5 flex justify-center items-center">
+              <EditAddressModal />
+            </div>
+          </div>
 
+          <div className="p-5 bg-green-100 space-y-2">
+            <h2 className="text-lg font-bold">Default Shipping & Billing Address</h2>
+            <p className="text-sm font-medium capitalize">Abdul Alim Emon</p>
+            <p className="text-sm font-medium capitalize">0172222222</p>
+            <p className="text-sm font-medium capitalize">mithapukur, Rangpur</p>
+            <p className="text-sm font-medium capitalize">mithapukur, Rangpur</p>
+            <div className="py-5 flex justify-center items-center">
+              <EditAddressModal />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
